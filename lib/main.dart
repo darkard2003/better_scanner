2be8f682cc18b86524dd41cd5438bdf4c9d55eff
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2024-12-30 20:52:21 +0530

    Added dynamic colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:better_scanner/screens/details_screen/details_screen.dart';
 import 'package:better_scanner/screens/qr_generator_screen/qr_generator.dart';
 import 'package:better_scanner/screens/scanner_screen/scanner_screen.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,22 +11,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Better Scanner',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
-        useMaterial3: true,
-      ),
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.cyan, brightness: Brightness.dark),
-      ),
-      routes: {
-        '/details': (context) => const DetailsScreen(),
-        '/generator': (context) => const QrGeneratorScreen(),
-        '/scanner': (context) => const ScannerScreen(),
-      },
-      home: const ScannerScreen(),
-    );
+    return DynamicColorBuilder(builder: (lightDynamic, darkDynamic) {
+      ColorScheme light, dark;
+      if (lightDynamic != null) {
+        light = lightDynamic.harmonized();
+      } else {
+        light = ColorScheme.fromSeed(seedColor: Colors.cyan);
+      }
+
+      if (darkDynamic != null) {
+        dark = darkDynamic.harmonized();
+      } else {
+        dark = ColorScheme.fromSeed(
+            seedColor: Colors.cyan, brightness: Brightness.dark);
+      }
+
+      return MaterialApp(
+        title: 'Better Scanner',
+        theme: ThemeData(
+          colorScheme: light,
+        ),
+        darkTheme: ThemeData(
+          colorScheme: dark,
+        ),
+        routes: {
+          '/details': (context) => const DetailsScreen(),
+          '/generator': (context) => const QrGeneratorScreen(),
+          '/scanner': (context) => const ScannerScreen(),
+        },
+        home: const ScannerScreen(),
+      );
+    });
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import dynamic_color
 import file_selector_macos
 import mobile_scanner
 import path_provider_foundation
@@ -12,6 +13,7 @@ import share_plus
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
